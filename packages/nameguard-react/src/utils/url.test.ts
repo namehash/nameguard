e7cb184d65d25612fbf23c7c69bd13b9a72fe2cf
FrancoAguzzi <frankind.eth@gmail.com>
2024-06-18 21:45:03 -0300

    chore: update url.ts test file after url.ts updates

--- packages/nameguard-react/src/utils/url.test.ts
+++ packages/nameguard-react/src/utils/url.test.ts
@@ -4,13 +4,13 @@ import { viewNameReportURL } from "./url";
 
 describe("viewNameReportURL", () => {
   it("should return correct NameGuard inspect name page URL for notrab.eth", () => {
-    const result = viewNameReportURL(buildENSName("notrab.eth"));
+    const result = viewNameReportURL(buildENSName("notrab.eth")).href;
     const expectedResult = "https://nameguard.io/inspect/notrab.eth";
 
     expect(result).toBe(expectedResult);
   });
   it("should return correct NameGuard inspect name page URL for 🐈‍⬛.eth", () => {
-    const result = viewNameReportURL(buildENSName("🐈‍⬛.eth"));
+    const result = viewNameReportURL(buildENSName("🐈‍⬛.eth")).href;
     const expectedResult = `https://nameguard.io/inspect/%F0%9F%90%88%E2%80%8D%E2%AC%9B.eth`;
 
     expect(result).toBe(expectedResult);
