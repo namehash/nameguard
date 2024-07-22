cce7409dce59444be56cb1ddd51e3921d7cab0c6
Carbon225 <carbon225@protonmail.com>
2024-07-22 18:41:35 +0200

    add grapheme unicode version

--- api/nameguard/nameguard.py
+++ api/nameguard/nameguard.py
@@ -283,6 +283,7 @@ class NameGuard:
                             risk_count=count_risks(grapheme_checks),
                             highest_risk=get_highest_risk(grapheme_checks),
                             grapheme_description=grapheme.description,
+                            unicode_version=grapheme.unicode_version,
                         )
                         for grapheme, grapheme_checks in zip(label_analysis.graphemes, label_graphemes_checks)
                     ]
@@ -363,6 +364,7 @@ class NameGuard:
             ),
             canonical_grapheme=label_analysis.canonical_label,
             grapheme_description=grapheme_analysis.description,
+            unicode_version=grapheme_analysis.unicode_version,
         )
 
     def _inspect_confusable(self, grapheme: InspectorConfusableGraphemeResult) -> ConfusableGuardReport:
@@ -380,6 +382,7 @@ class NameGuard:
             risk_count=count_risks(grapheme_checks),
             highest_risk=get_highest_risk(grapheme_checks),
             grapheme_description=grapheme.description,
+            unicode_version=grapheme.unicode_version,
             is_canonical=False,
         )
 
